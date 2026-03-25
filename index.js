5362487f91d60cfeb1d76056939734e86b317694
munirasheikh-rgb <munira.sheikh@student.moringaschool.com>
2026-03-25 16:23:11 +0300

    index.js

--- index.js
+++ index.js
@@ -4,27 +4,39 @@ let attemptCount = 0
  return ( passwordAttempt)=>{
   attemptCount++;
   
-  if(attemptCount >= 3 ){
-    return "Account locked too many attempts"
-  }
+
   if(passwordAttempt === userInfo.password){
-    return "login successful!"
+    return "Login successful"
   }
-  else{
-    return "login failed!"
+  if(attemptCount > 3){
+    return "Account locked due to too many failed login attempts"
   }
+  return `Attempt ${attemptCount}: Login failed`
+  
+  
+  
+
+  
+  
+  
+  
+  
+  }
+ 
+  
+
  }
-}
-const user={
-  username:"user 1",
-  password:"password 123"
 
-}
-const login = createLoginTracker(user)
+// const user={
+//   username:"user 1",
+//   password:"password 123"
 
-console.log(login(1))//first log in attempt failed
-console.log(login("password 123"))//login with user password (successfull output)
-console.log(login(2)) // after 3 attempts account is locked
+
+// const login = createLoginTracker(user)
+
+// console.log(login(1))//first log in attempt failed
+// console.log(login("password 123"))//login with user password (successfull output)
+// console.log(login(2)) // after 3 attempts account is locked
 
 
 module.exports = {
